e562f1e22be4abe4c2f7597139260981a4ac43ef
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2023-08-24 16:21:12 -0300

    upando projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,113 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import Profile from "./Matheus-picture.jpeg"
+import EmailIcon from "../Cornmanthe3rd-Metronome-Communication-gmail.512.png"
+import GitIcon from "../Arturo-Wibawa-Akar-Github.512.png"
+import LinkedinIcon from "../Danleech-Simple-Linkedin.512.png"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col">
+      
+      <div className="contato rounded-lg h-48 items-center justify-between bg-[url('../logo.png')]">
+        
+        <Image src={ Profile } alt="profile-picture" className="rounded-full ml-6" height="160"/>
+        <h1  className="text-7xl font-sans text-cyan-50">
+          Matheus Oliveira Lima
+        </h1>
+
+        <div id="contato" className="flex flex-col text-cyan-600 mr-6 bg-white rounded-lg">
+          Contato:
+          <ul className="list-disc list-inside ">
+            <li className="flex flex-row">
+              <Image src={ EmailIcon } alt="email-icon" className="h-6 w-6"/>
+              matheus.o.lima.95@gmail.com
+            </li>
+            <li className="flex flex-row">
+              <Image src={ LinkedinIcon } alt="linkedin-icon" className="h-6 w-6"/>
+              <Link href="https://www.linkedin.com/in/matheusoliveiralima/">
+                Linkedin: matheusoliveiralima
+              </Link>
+            </li>
+            <li className="flex flex-row">
+              <Image src={ GitIcon } alt="git-icon" className="h-6 w-6"/>
+              <Link href="https://github.com/matheusolilima">
+                Github: matheusolilima
+              </Link>
+            </li>
+          </ul>
         </div>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="objetivo-profissional rounded-lg mx-12">
+        <p className="text-left text-base break-normal">
+          Encontrei na programação uma forma de empregar meu raciocínio lógico, acho fascinante
+          como é possível usar essa ferramenta em tantos contextos diferentes para a resolução dos mais diversos problemas.
+          Tenho focado no aprimoramento de minhas hard-skills com React, Redux, Hooks, Jest, Node.js, MySQL, arquitetura MSC e TypeScript,
+          e nas soft-skills que tangem a comunicação e liderança.
+          Vamos nos conectar e ficarei feliz com a oportunidade de por em prática aquilo que venho estudando.
+          Encontrei na programação uma forma de empregar meu raciocínio lógico,
+          acho fascinante como é possível usar essa ferramenta em tantos contextos diferentes para a resolução dos mais diversos problemas.
+          Tenho focado no aprimoramento de minhas hard-skills com React, Redux, Hooks, Jest, Node.js, MySQL, arquitetura MSC e TypeScript,
+          e nas soft-skills que tangem a comunicação e liderança.
+          Vamos nos conectar e ficarei feliz com a oportunidade de por em prática aquilo que venho estudando.  
+        </p>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <div className="objetivo-profissional rounded-lg mx-12">
+        <h2>
+          Objetivo Profissional
+        </h2>
+        <p className="text-left text-base break-normal">
+          Buscar uma oportunidade como desenvolvedor web,
+          onde eu possa empregar minhas habilidades em programação e contribuir
+          de forma criativa para encontrar soluções eficientes para projetos web.
+          Almejo me aprimorar com ajuda da equipe e evoluir minhas habilidades,
+          sempre buscando novas maneiras de contribuir com a equipe.
+        </p>
+      </div>
+      
+      <div className="flex flex-row rounded-lg bg-amber-300 justify-center items-center h-96 mx-12 space-x-10">
+        
+        <div className="formacao rounded-lg">
+          <h2>
+            Formação
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <ul className="list-disc list-inside">
+            <li>Bacharel em Ciências da Computação – Universidade Federal de Jataí</li>
+            <li>Técnico em Informática - Instituto Federal de Goiás</li>
+            <li>Desenvolvimento Web Fullstack - Trybe</li>
+          </ul>
+        </div>
+        
+        <div className="tecnologias-habilidades rounded-lg">
+          <h2>
+            Principais tecnologias e habilidades 
+          </h2>
+          <ul className="list-disc list-inside">
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Python</li>
+              <li>SQL</li>
+              <li>POO</li>
+              <li>Scrum</li>
+            </ul>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      
+        <div className="experiencias rounded-lg">
+          <h2>
+            Experiências
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <ul className="list-disc list-inside">
+              <li>Professor de Programação</li>
+              <li>Assistente técnico em telecomunicações</li>
+            </ul>
+        </div>
+      
       </div>
     </main>
   )
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+		<div className="rounded-lg bg-amber-300 w-36 h-36">
+			<p>teste</p>
+		</div>
+	)
+}
