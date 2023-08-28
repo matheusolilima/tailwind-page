83f1c49ed330608c98e55436218b89ad2cb668b5
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2023-08-28 17:46:18 -0300

    atualizaçao de  layout em andamento

--- src/app/page.js
+++ src/app/page.js
@@ -9,64 +9,71 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       
-      <div className="contato rounded-lg h-48 items-center justify-between bg-[url('../logo.png')]">
+      <div className="flex bg-cyan-700 rounded-lg h-24 items-center justify-between ">
         
-        <Image src={ Profile } alt="profile-picture" className="rounded-full ml-6" height="160"/>
+        
         <h1  className="text-7xl font-sans text-cyan-50">
           Matheus Oliveira Lima
         </h1>
 
-        <div id="contato" className="flex flex-col text-cyan-600 mr-6 bg-white rounded-lg">
+        <div  className="flex flex-col text-cyan-600 mr-6 bg-white rounded-lg">
           Contato:
-          <ul className="list-disc list-inside ">
-            <li className="flex flex-row">
+          
+            <div className="flex flex-row">
               <Image src={ EmailIcon } alt="email-icon" className="h-6 w-6"/>
               matheus.o.lima.95@gmail.com
-            </li>
-            <li className="flex flex-row">
+            </div>
+            <div className="flex flex-row">
               <Image src={ LinkedinIcon } alt="linkedin-icon" className="h-6 w-6"/>
               <Link href="https://www.linkedin.com/in/matheusoliveiralima/">
                 Linkedin: matheusoliveiralima
               </Link>
-            </li>
-            <li className="flex flex-row">
+            </div>
+            <div className="flex flex-row">
               <Image src={ GitIcon } alt="git-icon" className="h-6 w-6"/>
               <Link href="https://github.com/matheusolilima">
                 Github: matheusolilima
               </Link>
-            </li>
-          </ul>
+            </div>
+          
         </div>
 
       </div>
 
-      <div className="objetivo-profissional rounded-lg mx-12">
-        <p className="text-left text-base break-normal">
-          Encontrei na programação uma forma de empregar meu raciocínio lógico, acho fascinante
-          como é possível usar essa ferramenta em tantos contextos diferentes para a resolução dos mais diversos problemas.
-          Tenho focado no aprimoramento de minhas hard-skills com React, Redux, Hooks, Jest, Node.js, MySQL, arquitetura MSC e TypeScript,
-          e nas soft-skills que tangem a comunicação e liderança.
-          Vamos nos conectar e ficarei feliz com a oportunidade de por em prática aquilo que venho estudando.
-          Encontrei na programação uma forma de empregar meu raciocínio lógico,
-          acho fascinante como é possível usar essa ferramenta em tantos contextos diferentes para a resolução dos mais diversos problemas.
-          Tenho focado no aprimoramento de minhas hard-skills com React, Redux, Hooks, Jest, Node.js, MySQL, arquitetura MSC e TypeScript,
-          e nas soft-skills que tangem a comunicação e liderança.
-          Vamos nos conectar e ficarei feliz com a oportunidade de por em prática aquilo que venho estudando.  
-        </p>
-      </div>
+      <div className="flex flex-row">
+        <Image src={ Profile } alt="profile-picture" className="cropped1 w-8/12 "/>
 
-      <div className="objetivo-profissional rounded-lg mx-12">
-        <h2>
-          Objetivo Profissional
-        </h2>
-        <p className="text-left text-base break-normal">
-          Buscar uma oportunidade como desenvolvedor web,
-          onde eu possa empregar minhas habilidades em programação e contribuir
-          de forma criativa para encontrar soluções eficientes para projetos web.
-          Almejo me aprimorar com ajuda da equipe e evoluir minhas habilidades,
-          sempre buscando novas maneiras de contribuir com a equipe.
-        </p>
+        <div className="flex flex-col rounded-lg bg-black w-4/12">
+
+<div className="sobre rounded-lg mx-12">
+  <h2>
+    Sobre
+  </h2>
+  <p className="text-left text-base break-normal">
+    Encontrei na programação uma forma de empregar meu raciocínio lógico, acho fascinante
+    como é possível usar essa ferramenta em tantos contextos diferentes para a resolução dos mais diversos problemas.
+    Tenho focado no aprimoramento de minhas hard-skills com React, Redux, Hooks, Jest, Node.js, MySQL, arquitetura MSC e TypeScript,
+    e nas soft-skills que tangem a comunicação e liderança.
+    Vamos nos conectar e ficarei feliz com a oportunidade de por em prática aquilo que venho estudando.  
+  </p>
+</div>
+
+<div className="objetivo-profissional rounded-lg mx-12">
+  <h2>
+    Objetivo Profissional
+  </h2>
+  <p className="text-left text-base break-normal">
+    Buscar uma oportunidade como desenvolvedor web,
+    onde eu possa empregar minhas habilidades em programação e contribuir
+    de forma criativa para encontrar soluções eficientes para projetos web.
+    Almejo me aprimorar com ajuda da equipe e evoluir minhas habilidades,
+    sempre buscando novas maneiras de contribuir com a equipe.
+  </p>
+</div>
+</div>
       </div>
+      
+      
       
       <div className="flex flex-row rounded-lg bg-amber-300 justify-center items-center h-96 mx-12 space-x-10">
         
