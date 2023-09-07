cf712b651f7682e29f0635f1e9e455449eaf79e8
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2023-09-07 15:54:31 -0300

    alternancia clicando nos elementos

--- src/app/page.js
+++ src/app/page.js
@@ -15,12 +15,23 @@ import FrameCard from '@/components/frameCard'
 export default function Home() {
   // posso armazenar componente em variavel pra renderizar quando quero
   // para adicionar cor especifica usa-se bg-[#50d71e]
-  const [ isSobreVisible, setSobreVisible] = React.useState(true);
-  const [ isObjetivoVisible, setObjetivoVisible] = React.useState(false);
+  const [ sobre, setSobre] = React.useState({
+    title: "Sobre",
+    cor: "bg-blue-400",
+    isVisible: true,
+    list:["Encontrei na programação uma forma de empregar meu raciocínio lógico, acho fascinante como é possível usar essa ferramenta em tantos contextos diferentes para a resolução dos mais diversos problemas. Tenho focado no aprimoramento de minhas hard-skills com React, Redux, Hooks, Jest, Node.js, MySQL, arquitetura MSC e TypeScript, e nas soft-skills que tangem a comunicação e liderança. Vamos nos conectar e ficarei feliz com a oportunidade de por em prática aquilo que venho estudando."]
+  });
+
+  const [ objetivo, setObjetivo] = React.useState({
+    title: "Objetivo",
+    cor: "bg-blue-400",
+    isVisible: false,
+    list:["Buscar uma oportunidade como desenvolvedor web, onde eu possa empregar minhas habilidades em programação e contribuir de forma criativa para encontrar soluções eficientes para projetos web. Almejo me aprimorar com ajuda da equipe e evoluir minhas habilidades, sempre buscando novas maneiras de contribuir com a equipe."]
+  });
   
-  const [ isFormacaoVisible, setFormacaoVisible] = React.useState({
+  const [ formacao, setFormacao] = React.useState({
     title: "Formação",
-    classes: "bg-green-600",
+    cor: "bg-green-600",
     isVisible: true,
     list:[
       "Bacharel em Ciências da Computação – Universidade Federal de Jataí",
@@ -29,9 +40,9 @@ export default function Home() {
     ]
   });
   
-  const [ isHabilidadesVisible, setHabilidadesVisible] = React.useState({
-    title: "Tecnologias e habilidades ",
-    classes: "bg-red-600",
+  const [ habilidades, setHabilidades] = React.useState({
+    title: "Tecnologias e habilidades",
+    cor: "bg-red-600",
     isVisible: false,
     list:[
       "HTML","CSS","JavaScript","React",
@@ -40,54 +51,30 @@ export default function Home() {
     ]
   });
 
-  const [ isExperienciasVisible, setExperienciasVisible] = React.useState(false);
-
-  const sobre = {
-    title: "Sobre",
-    classes: "bg-blue-400",
-    list:["Encontrei na programação uma forma de empregar meu raciocínio lógico, acho fascinante como é possível usar essa ferramenta em tantos contextos diferentes para a resolução dos mais diversos problemas. Tenho focado no aprimoramento de minhas hard-skills com React, Redux, Hooks, Jest, Node.js, MySQL, arquitetura MSC e TypeScript, e nas soft-skills que tangem a comunicação e liderança. Vamos nos conectar e ficarei feliz com a oportunidade de por em prática aquilo que venho estudando."]
-  }
-  const objetivo = {
-    title: "Objetivo",
-    classes: "bg-blue-400",
-    list:["Buscar uma oportunidade como desenvolvedor web, onde eu possa empregar minhas habilidades em programação e contribuir de forma criativa para encontrar soluções eficientes para projetos web. Almejo me aprimorar com ajuda da equipe e evoluir minhas habilidades, sempre buscando novas maneiras de contribuir com a equipe."]
-  }
-  const formacao = {
-    title: "Formação",
-    classes: "bg-green-600",
-    list:[
-      "Bacharel em Ciências da Computação – Universidade Federal de Jataí",
-      "Técnico em Informática - Instituto Federal de Goiás",
-      "Desenvolvimento Web Fullstack - Trybe"
-    ]
-  }
-
-  const habilidades = {
-    title: "Tecnologias e habilidades ",
-    classes: "bg-red-600",
-    list:[
-      "HTML","CSS","JavaScript","React",
-      "Python","SQL","NoSQL","Node",
-      "Jest","GitHub","POO","Scrum"
-    ]
-  }
-
-  const experiencias = {
-    title: "Experiências ",
-    classes: "bg-blue-600",
+  const [ experiencias, setExperiencias] = React.useState({
+    title: "Experiências",
+    cor: "bg-blue-600",
+    isVisible: false,
     list:["Professor de Programação","Assistente técnico em telecomunicações"]
-  }
+  });
 
   function toggleSobreObjetivo(){
-    // const id = event.target.id
-    setSobreVisible(!isSobreVisible)
-    setObjetivoVisible(!isObjetivoVisible)
+    setSobre({ ...sobre, isVisible: !sobre.isVisible})
+    setObjetivo({ ...objetivo, isVisible: !objetivo.isVisible})
   }
 
   function toggleForHabExp(){
-    setFormacaoVisible({ isVisible: !(isFormacaoVisible.isVisible) })
-    setHabilidadesVisible({ isVisible: !(isHabilidadesVisible.isVisible) })
-    setExperienciasVisible({ isVisible: !(isExperienciasVisible.isVisible) })
+    const displaying = [
+      formacao.isVisible,
+      habilidades.isVisible,
+      experiencias.isVisible
+    ]
+    
+    const aux = habilidades.isVisible
+    setFormacao({ ...formacao, isVisible: displaying[2]})
+    setHabilidades({ ...habilidades, isVisible: displaying[0]})
+    setExperiencias({ ...experiencias, isVisible: aux})
+
   }
 
   return (
@@ -101,16 +88,14 @@ export default function Home() {
         
         <Image src={ Logo } alt="logo" className="flex h-[80px] w-[240px] rounded-lg"/>
         
-        {/* ContatoCard() */}
-        {MenuButtons([formacao,habilidades,experiencias,sobre,objetivo])}  
+        {MenuButtons([formacao,habilidades,experiencias,sobre,objetivo])}
+        {/* <MenuButtons />
+        <MenuButtons /> */}
       </div>
-      {/* MenuButtons([formacao,habilidades,experiencias,sobre,objetivo]) */}
-      
+
       <div className="flex flex-row">
         
-        {/* <Image src={ Profile } alt="profile-picture" className="h-1/5 w-1/5 rounded-lg mx-12"/> */}
-        
-        {FrameCard()}
+        <FrameCard />
         
 
         <div className={`flex flex-row rounded-lg bg-black mx-12 w-2/3`}>
@@ -118,7 +103,7 @@ export default function Home() {
           <div
             id="sobre"
             onClick={toggleSobreObjetivo}
-            className={`${isSobreVisible? "": "hidden"} sobre-objetivo rounded-lg mx-[10px]`}
+            className={`${sobre.isVisible? "": "hidden"} sobre-objetivo rounded-lg mx-[10px]`}
             >
               <h2>Sobre</h2>
               <p className="text-left text-base break-normal">
@@ -133,7 +118,7 @@ export default function Home() {
           <div
             id="objetivo"
             onClick={toggleSobreObjetivo}
-            className={`${isObjetivoVisible? "": "hidden"} sobre-objetivo rounded-lg mx-[10px]`}
+            className={`${objetivo.isVisible? "": "hidden"} sobre-objetivo rounded-lg mx-[10px]`}
             >
               <h2>
                 Objetivo Profissional
@@ -147,17 +132,23 @@ export default function Home() {
               </p>
           </div>
           
-          {InfoCard(isFormacaoVisible)}
-          {/* InfoCard(isHabilidadesVisible) */}
+          {/* <InfoCard infos={sobre} toggler={toggleSobreObjetivo}/> */}
+          {/* <InfoCard infos={objetivo} toggler={toggleSobreObjetivo}/> */}
+          {/* funciona mas o tamanho fica ruim*/}
+
+          <InfoCard infos={formacao} toggler={toggleForHabExp}/>
+          <InfoCard infos={habilidades} toggler={toggleForHabExp}/>
+          <InfoCard infos={experiencias} toggler={toggleForHabExp}/>
+          
         </div>
       </div>
-      <div className="flex flex-row rounded-lg bg-amber-300 justify-center items-center h-[320px] mx-12 space-x-10">
+      {/* <div className="flex flex-row rounded-lg bg-amber-300 justify-center items-center h-[320px] mx-12 space-x-10">
         
         {InfoCard(formacao)}
         {InfoCard(habilidades)}
         {InfoCard(experiencias)}
       
-      </div>
+      </div> */}
 
     </main>
   )
--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -1,19 +1,14 @@
-export default function InfoCard(infos) {
-  const localInfo = {
-    cols:""
-  }
-  
-  // localInfo.cols = `grid-cols-${(infos.list.length / 4)}`
-  
-  if (infos.list.length > 3) localInfo.cols = "grid-cols-2"
-  const toggler = `${infos.isVisible? "": "hidden"}` 
+export default function InfoCard({ infos, toggler }) {
+
   return (
-    <div className={`flex justify-evenly flex-wrap rounded-lg p-[20px] w-[300px] h-[300px] ${ infos.classes } my-[20px] mx-[10px]`}>
+    <div
+      onClick={ toggler }
+      className={`${infos.isVisible? "": "hidden"} flex justify-evenly flex-wrap rounded-lg p-[20px] w-[300px] h-[300px] ${ infos.cor } my-[20px] mx-[10px]`}>
       <h2>{infos.title}</h2>
 
-      <div className={`flex grid ${ localInfo.cols } justify-evenly flex-wrap`}>
+      <div className={`flex grid ${ infos.list.length > 3? "grid-cols-2": ""} justify-evenly flex-wrap`}>
 
-        {infos.list.map((item) => (
+        {infos.list && infos.list.map((item) => (
           <div key={item} className="rounded-lg mx-2">{ item }</div>
         ))}
         {/*coloquei margin horizontal pra quando tiver coluna espaçar */}
